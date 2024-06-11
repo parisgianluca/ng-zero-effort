cf9d6cf642c07d8dbdf2a2e91e4ccac5300921ae
Gianluca Paris <gianluca.paris@outlook.com>
2024-06-11 18:13:49 +0200

    feat(app): add auth routes and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/landing/landing.component').then(
+        (c) => c.LandingComponent
+      ),
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./components/auth/login/login.component').then(
+            (c) => c.LoginComponent
+          ),
+      },
+      {
+        path: 'sign-up',
+        loadComponent: () =>
+          import('./components/auth/sign-up/sign-up.component').then(
+            (c) => c.SignUpComponent
+          ),
+      },
+      {
+        path: 'reset-password',
+        loadComponent: () =>
+          import(
+            './components/auth/reset-password/reset-password.component'
+          ).then((c) => c.ResetPasswordComponent),
+      },
+    ],
+  },
+];
