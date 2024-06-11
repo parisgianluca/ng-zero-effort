5024fd1f84ac4a4884abf4672ef6eeb924858920
Gianluca Paris <gianluca.paris@outlook.com>
2024-06-11 22:14:42 +0200

    feat(login): add login page

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onLogin() {
+    console.log('login');
+  }
+}
